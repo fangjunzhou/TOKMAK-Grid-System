fffced8cdf606e57f2ad286608940bb4ac9e49f2
Fishball_Official <fangjun.zhou02@gmail.com>
2021-04-21 05:53:37 +0800

    Add the GridSystem starting code
    Signed-off-by: Fishball_Official <fangjun.zhou02@gmail.com>

---
+++ Assets/fangjun/GridSystem/Scripts/Edge.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GridSystem
+{
+    public class Edge
+    {
+
+        #region Private Field
+
+        // the basic info of the edge
+        private Vertex m_from;
+        private Vertex m_to;
+        private float m_cost;
+
+        #endregion
+
+        #region Public Field
+
+        /// <summary>
+        /// The start vertex of the edge
+        /// </summary>
+        public Vertex from
+        {
+            get
+            {
+                return m_from;
+            }
+            set
+            {
+                m_from = value;
+            }
+        }
+
+        /// <summary>
+        /// The end vertex of the edge
+        /// </summary>
+        public Vertex to
+        {
+            get
+            {
+                return m_to;
+            }
+            set
+            {
+                m_to = value;
+            }
+        }
+
+        /// <summary>
+        /// The cost of this edge
+        /// </summary>
+        public float cost
+        {
+            get
+            {
+                return m_cost;
+            }
+            set
+            {
+                m_cost = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// The basic constructor of an Edge
+        /// </summary>
+        /// <param name="from">The start vertex of the edge</param>
+        /// <param name="to">The end vertex of the edge</param>
+        /// <param name="cost">the cost of current edge</param>
+        public Edge(Vertex from, Vertex to, float cost)
+        {
+            m_from = from;
+            m_to = to;
+            m_cost = cost;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/fangjun/GridSystem/Scripts/TestCode/TestGridCoordinate.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using GridSystem;
+
+/// <summary>
+/// The test class for the GridCoordinate class
+/// </summary>
+public class TestGridCoordinate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    [Button("Test default constructor for GridCoordinate")]
+    /// <summary>
+    /// Test the default constructor for the GridCoordinate class
+    /// </summary>
+    private void testDefaultConstructor()
+    {
+        GridCoordinate gridCoordinate = new GridCoordinate();
+        Debug.Log("Default constructor: " + gridCoordinate.ToString());
+    }
+
+    [Button("Test constructor with value for GridCoordinate")]
+    /// <summary>
+    /// Test the constructor with value for GridCoordinate
+    /// </summary>
+    private void testConstructorWithValue()
+    {
+        GridCoordinate gridCoordinate = new GridCoordinate(1, 1);
+        Debug.Log("Constructor initialize as (1, 1): " + gridCoordinate.ToString());
+    }
+
+    [Button("Test get and set for GridCoordinate")]
+    /// <summary>
+    /// Test the get ans set function for GridCoordiante
+    /// </summary>
+    private void testGetandSet()
+    {
+        GridCoordinate gridCoordinate = new GridCoordinate();
+        Debug.Log("Default constructor: " + gridCoordinate.ToString());
+        gridCoordinate.x = 10;
+        gridCoordinate.y = 10;
+        Debug.Log("Set the x and y to 10: " + gridCoordinate.ToString());
+        Debug.Log("Get x: " + gridCoordinate.x.ToString());
+        Debug.Log("Get y: " + gridCoordinate.y.ToString());
+    }
+}
