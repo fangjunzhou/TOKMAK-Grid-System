1b33342dd43ef43f62fba7b7a7cd858cd35cac0b
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-04-28 04:19:22 +0800

    Add the SquareGridSystem that inherite from GridSystem
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

---
+++ Assets/fangjun/GridSystem/Scripts/Square Grid/SquareGridSystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GridSystem
+{
+    public class SquareGridSystem<DataType> : IGridSystem<DataType>
+    {
+
+        #region Public Field
+        #endregion
+
+        #region Private Field
+        #endregion
+
+        #region Consttuctor
+        #endregion
+
+
+        #region IGridSystem interface
+
+        public void AddDoubleEdge(GridCoordinate coordinate1, GridCoordinate coordinate2, float weight)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddEdge(GridCoordinate start, GridCoordinate end, float weight)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddVertex(GridCoordinate coordinate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Vertex<DataType>> FindShortestPath(GridCoordinate start, GridCoordinate end)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public float GetEdge(GridCoordinate start, GridCoordinate end)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vertex<DataType> GetVertex(GridCoordinate coordinate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveDoubleEdge(GridCoordinate coordinate1, GridCoordinate coordinate2)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveEdge(GridCoordinate start, GridCoordinate end)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveVertex(GridCoordinate coordinate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/fangjun/GridSystem/Scripts/Square Grid/SquareGridVertex.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GridSystem
+{
+    /// <summary>
+    /// The SquareGridVertex extention of Vertex class
+    /// each SquareGridVertex have four dirction
+    /// up, down, left, and right
+    /// </summary>
+    public class SquareGridVertex<DataType> : Vertex<DataType>
+    {
+        #region Constructor
+
+        /// <summary>
+        /// The basic constructor of SquareGridVertex
+        /// </summary>
+        public SquareGridVertex()
+        {
+            // set the coordinate to (0, 0)
+            m_coordinate = new GridCoordinate();
+            // set the cost to 0
+            m_cost = 0;
+            // initialize the connection dic to be empty
+            connection = new Dictionary<string, Edge<DataType>>();
+
+            // initialize the connection
+            SquareGridSetup();
+        }
+
+        /// <summary>
+        /// The constructor with coordinate initialized
+        /// </summary>
+        /// <param name="coordinate">the coordinate of current vertex</param>
+        public SquareGridVertex(GridCoordinate coordinate)
+        {
+
+            // set the coordinate
+            this.m_coordinate = coordinate;
+            // set the cost to 0
+            m_cost = 0;
+            // initialize the connection dic to be empty
+            connection = new Dictionary<string, Edge<DataType>>();
+
+            // initialize the connection
+            SquareGridSetup();
+        }
+
+        /// <summary>
+        /// The constructor with cost initialized
+        /// </summary>
+        /// <param name="cost">the cost of current vertex</param>
+        public SquareGridVertex(float cost)
+        {
+            // set the coordinate to (0, 0)
+            m_coordinate = new GridCoordinate();
+            // set the cost
+            this.m_cost = cost;
+            // initialize the connection dic to be empty
+            connection = new Dictionary<string, Edge<DataType>>();
+
+            // initialize the connection
+            SquareGridSetup();
+        }
+
+        /// <summary>
+        /// The constructor with coordinate and cost initialized
+        /// </summary>
+        /// <param name="coordinate">the coordinate of current vertex</param>
+        /// <param name="cost">the cost of current vertex</param>
+        public SquareGridVertex(GridCoordinate coordinate, float cost)
+        {
+            // set the coordinate
+            this.m_coordinate = coordinate;
+            // set the cost
+            this.m_cost = cost;
+            // initialize the connection dic to be empty
+            connection = new Dictionary<string, Edge<DataType>>();
+
+            // initialize the connection
+            SquareGridSetup();
+        }
+
+        #endregion
+
+        #region Private Field
+
+        /// <summary>
+        /// Setup the connection dictionary of SquareGridVetex with four directions
+        /// </summary>
+        private void SquareGridSetup()
+        {
+            AddConnectionDir("up");
+            AddConnectionDir("down");
+            AddConnectionDir("left");
+            AddConnectionDir("right");
+        }
+
+        #endregion
+    }
+}
