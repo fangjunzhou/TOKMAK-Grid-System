c88873cf73567e0e4862b4f079db46e744b1d352
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-06 19:51:29 +0800

    Add two action events to the IGridEventHandler class
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

--- Assets/fangjun/GridSystem/Scripts/Core/IGridEventHandler.cs
+++ Assets/fangjun/GridSystem/Scripts/Core/IGridEventHandler.cs
@@ -1,7 +1,22 @@
-﻿namespace GridSystem
+﻿using System;
+using UnityEngine;
+
+namespace GridSystem
 {
     public interface IGridEventHandler
     {
+        #region Public Field
+
+        /// <summary>
+        /// The GameObject that current cursor selected
+        /// </summary>
+        GameObject currentGridObject { get; set; }
         
+        /// <summary>
+        /// The event triggered when currentGridObject changes
+        /// </summary>
+        Action updateSelectedGrid { get; set; }
+
+        #endregion
     }
 }
