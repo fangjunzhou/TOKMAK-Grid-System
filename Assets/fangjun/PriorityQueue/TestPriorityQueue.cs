7075a666c52e0be794a34f8e2c92248770fe0d71
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-07 21:28:33 +0800

    Add the PriorityQueue to the project Closes Fangjun_Zhou/gridsystem#3
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

---
+++ Assets/fangjun/PriorityQueue/TestPriorityQueue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace fangjun.PriorityQueue
+{
+    public class TestPriorityQueue : MonoBehaviour
+    {
+        #region Public Field
+        public TMP_Text text;
+        public TMP_InputField inputString;
+        public TMP_InputField inputPriority;
+        #endregion
+
+        #region Private Field
+        private PriorityQueue<string> priorityQueue = new PriorityQueue<string>();
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            text.text = priorityQueue.ToString();
+        }
+
+        #region Public Methods
+        public void Push()
+        {
+            priorityQueue.push(new PriorityQueueNode<string>()
+            {
+                data = inputString.text,
+                priority = int.Parse(inputPriority.text)
+            });
+        }
+
+        public void Pop()
+        {
+            Debug.Log(priorityQueue.pop());
+        }
+        #endregion
+    }
+}
