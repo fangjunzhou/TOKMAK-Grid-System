ce1f0b6bdfbfb356a1ee8f2d3120dc578044d666
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-07 14:47:43 +0800

    Implement new OnCoordinateChange callback function in the interface after modification
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

--- Assets/fangjun/GridSystem/Scripts/TestCode/SquareGrid/TestSquareGridGenerator.cs
+++ Assets/fangjun/GridSystem/Scripts/TestCode/SquareGrid/TestSquareGridGenerator.cs
@@ -20,7 +20,7 @@ public class TestSquareGridGenerator : MonoBehaviour
     [Button("Test the GenerateMap method")]
     private void TestGenerate()
     {
-        squareGridGenerator.GenerateMap(width, height);
+        squareGridGenerator.GenerateMap<SampleSquareGridElement>(width, height, 2);
     }
 
     [Button("Test the Clear method")]
