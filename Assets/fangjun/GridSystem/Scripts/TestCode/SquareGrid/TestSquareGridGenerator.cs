a5e900149812a721e7c0415d917a8506c9cfa80b
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-09 14:27:35 +0800

    Update DrawObstcale sample class. Now can detect opposite corners
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

--- Assets/fangjun/GridSystem/Scripts/TestCode/SquareGrid/TestSquareGridGenerator.cs
+++ Assets/fangjun/GridSystem/Scripts/TestCode/SquareGrid/TestSquareGridGenerator.cs
@@ -20,7 +20,7 @@ public class TestSquareGridGenerator : MonoBehaviour
     [Button("Test the GenerateMap method")]
     private void TestGenerate()
     {
-        squareGridGenerator.GenerateMap<SampleSquareGridElement>(width, height, 2);
+        squareGridGenerator.GenerateMap<SampleSquareGridElement>(width, height, 1);
     }
 
     [Button("Test the Clear method")]
