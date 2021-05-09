7f55bf34877432e22ebb193ca4371aaa378d4684
AN <genesis_an@outlook.com>
2021-05-09 17:26:51 +0800

    Update AutoUpdate.bat

--- docfx_project/AutoUpdate.bat
+++ docfx_project/AutoUpdate.bat
@@ -1,2 +1,2 @@
 SET PPath=%~dp0
-C:\Users\AN\AppData\Local\Programs\WinSCP\WinSCP.exe /console /command "option batch continue" "option confirm off" "open sftp://uploader:an123456@anw.noip.cn:22 -hostkey="ssh-ed25519 255 Ove7aJ2FwQP++qbJNEK8CilsnU/MVXhYJ7glubchqBE="" "option transfer binary" "put %PPath%_site\* /var/www/html/doc/gridsystem/" "exit" /log=log_file.txt
+C:\Users\AN\AppData\Local\Programs\WinSCP\WinSCP.exe /console /command "option batch continue" "option confirm off" "open sftp://uploader:an123456@anw.noip.cn:22 -hostkey="ssh-ed25519 255 Ove7aJ2FwQP++qbJNEK8CilsnU/MVXhYJ7glubchqBE="" "option transfer binary" "put %PPath%_site\* /var/www/html/doc/gridsystem/" "exit" /log=%PPath%log_file.txt
