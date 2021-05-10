3b24d80513b0262cf486c81d09c6798a3dd142fa
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-10 23:00:24 +0800

    Change update.bat

--- docfx_project/AutoUpdate.bat
+++ docfx_project/AutoUpdate.bat
@@ -1,3 +1,1 @@
-SET PPath=%~dp0
-WinSCP /console /command "option batch continue" "option confirm off" "open sftp://uploader:an123456@anw.noip.cn:22 -hostkey="ssh-ed25519 255 Ove7aJ2FwQP++qbJNEK8CilsnU/MVXhYJ7glubchqBE="" "option transfer binary" "put %PPath%_site\* /var/www/html/doc/gridsystem/" "exit" /log=%PPath%log_file.txt
-EXIT /B 0
+WinSCP /ini=nul /script=".\script.txt
--- docfx_project/BuildDoc.bat
+++ docfx_project/BuildDoc.bat
@@ -1,3 +1,3 @@
 ::项目目录
 SET PPath=%~dp0
-docfx "%PPath%docfx.json" >>log.txt
+docfx "%PPath%docfx.json"
