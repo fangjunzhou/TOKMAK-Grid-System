4824701ba8910402604293b9bb521007d2e0569f
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-11 18:57:43 +0800

    Changed VertexData and TestVertexData Finish implementing the VertexData wrapper class and its test class
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

--- Runtime/Core/GridDataIO/VertexData.cs
+++ Runtime/Core/GridDataIO/VertexData.cs
@@ -1,7 +1,107 @@
-﻿namespace FinTOKMAK.GridSystem.GridDataIO
+﻿using System;
+
+namespace FinTOKMAK.GridSystem
 {
-    public class VertexData
+    /// <summary>
+    /// Wrapper class that stores the coordinate, cost, and edge data
+    /// </summary>
+    /// <typeparam name="DataType">the data type of Vertex</typeparam>
+    [System.Serializable]
+    public class VertexData<DataType>
     {
+        #region Public Field
         
+        /// <summary>
+        /// The coordinate of the Vertex
+        /// coordinate[0] is the x coordinate
+        /// coordinate[1] is the y coordinate
+        /// </summary>
+        public int[] coordinate = new int[2];
+
+        /// <summary>
+        /// the cost of the Vertex
+        /// </summary>
+        public float cost;
+
+        /// <summary>
+        /// A list of edge targets;
+        /// </summary>
+        public int[][] edgeTargets;
+        /// <summary>
+        /// The cost of edge between current Vertex and Vertex nearby
+        /// -1 if there's no connection in certain direction
+        /// </summary>
+        public float[] edgeCost;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// The basic VertexData constructor
+        /// </summary>
+        /// <param name="vertex">the source Vertex to generate a correspond VertexData</param>
+        public VertexData(Vertex<DataType> vertex)
+        {
+            // set the coordinate of the VertexData
+            this.coordinate[0] = vertex.coordinate.x;
+            this.coordinate[1] = vertex.coordinate.y;
+            
+            // set the cost of VertexData
+            this.cost = vertex.cost;
+            
+            // set the edge information of VertexData
+            this.edgeCost = new float[vertex.connection.Count];
+            this.edgeTargets = new int[vertex.connection.Count][];
+            // get all the connection information from the Vertex
+            int index = 0;
+            foreach (string direction in vertex.connection.Keys)
+            {
+                // if there's no connection in this direction
+                if (vertex.connection[direction] == null)
+                {
+                    this.edgeCost[index] = -1;
+                }
+                else
+                {
+                    // set the edgeCost
+                    this.edgeCost[index] = vertex.connection[direction].cost;
+                    // set the edgeDirection
+                    this.edgeTargets[index] = new[]
+                    {
+                        vertex.connection[direction].to.coordinate.x,
+                        vertex.connection[direction].to.coordinate.y
+                    };
+                }
+
+                index++;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Override the ToString method for Debugging
+        /// </summary>
+        /// <returns>the string representation of current VertexData</returns>
+        public override string ToString()
+        {
+            string res = "";
+            res += "Coordinate: " + "(" + coordinate[0] + ", " + coordinate[1] + ")\n";
+            res += "Cost: " + cost + "\n";
+            res += "Edge targets: \n";
+            foreach (int[] targetCoordinate in edgeTargets)
+            {
+                res += "(" + targetCoordinate[0] + ", " + targetCoordinate[1] + ")\n";
+            }
+
+            res += "Edge costs: \n";
+            foreach (float cost in edgeCost)
+            {
+                res += cost + "\n";
+            }
+
+            return res;
+        }
     }
 }
--- Tests/Runtime/Core/TestVertexData.cs
+++ Tests/Runtime/Core/TestVertexData.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
+using NaughtyAttributes;
 
 namespace FinTOKMAK.GridSystem.Core.Test
 {
     public class TestVertexData : MonoBehaviour
     {
-        
+        [Button("Test the constructor of VertexData")]
+        private void TestVertexDataConstructor()
+        {
+            // create two vertices and connect them
+            Vertex<string> vertex1 = new Vertex<string>(new GridCoordinate(0, 0));
+            Vertex<string> vertex2 = new Vertex<string>(new GridCoordinate(1, 0));
+            vertex1.AddConnectionDir("right");
+            vertex2.AddConnectionDir("left");
+            vertex1.SetDoubleConnection("right", vertex2, "left", 10);
+            
+            Debug.Log("Vertex 1:");
+            Debug.Log(vertex1);
+            Debug.Log("Vertex 2:");
+            Debug.Log(vertex2);
+
+            VertexData<string> vertexData = new VertexData<string>(vertex1);
+            
+            Debug.Log("VertexData: " + vertexData);
+        }
     }
 }
